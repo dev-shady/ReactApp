406c21ae26bba41601a862401207dd3ce7e427ff
Amit Kumar <itsdevshady@gmail.com>
2025-11-13 16:13:30 +0530

    added ALL filter for restaurants

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,13 +1,35 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantMockData } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [restaurants, setRestaurants] = useState(
+    restaurantMockData.restaurants
+  );
+
   return (
     <div className="body">
-      <div className="search">Search Bar</div>
+      <div className="filters">
+        <button
+          onClick={() => {
+            const filteredRest = restaurants.filter(
+              (restaurant) => restaurant.info.avgRating >= 4.5
+            );
+            setRestaurants(filteredRest);
+          }}
+        >
+          Top Rated
+        </button>
+        <button
+          className="filter-item"
+          onClick={() => setRestaurants(restaurantMockData.restaurants)}
+        >
+          All
+        </button>
+      </div>
 
       <div className="res-container">
-        {restaurantMockData.restaurants.map((item) => {
+        {restaurants.map((item) => {
           return <RestaurantCard key={item.info.id} data={item.info} />;
         })}
       </div>
