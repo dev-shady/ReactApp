0d2ddae61ed5e78eeac1110dc7d3ef8c78927e8c
Amit Kumar <itsdevshady@gmail.com>
2025-11-13 22:08:46 +0530

    fetch data dynamically from api

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,11 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantMockData } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SWIGGY_API_URL } from "../utils/constant";
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(
-    restaurantMockData.restaurants
-  );
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(SWIGGY_API_URL);
+      const data = await response.json();
+      //handle index out of bound for cards[1]
+      const restaurants =
+        data?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants;
+      setRestaurants(restaurants);
+    } catch (err) {
+      console.log("error ", err);
+    }
+  };
 
   return (
     <div className="body">
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,3 +3,7 @@ export const LOGO_URL =
 
 export const MEDIA_BASE_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+// {data-cards[1]-card-card-gridElements-infoWithStyle-restaurants}
+export const SWIGGY_API_URL =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.63270&lng=77.21980&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
