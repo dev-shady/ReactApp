69cc9d4d7dec20120e6d3129629be4a7b8acaef7
Amit Kumar <itsdevshady@gmail.com>
2025-11-14 15:20:59 +0530

    add routing config using react-router-dom library

--- src/component/index.js
+++ src/component/index.js
@@ -3,15 +3,34 @@ import ReactDOM from "react-dom/client";
 import "../../index.css";
 import Header from "./Header";
 import Body from "./Body";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import About from "./About";
 
 const AppComponent = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppComponent />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppComponent />);
+root.render(<RouterProvider router={appRouter} />);
