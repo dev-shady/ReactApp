e149b11562ddcb058297c15af7696c3466027a83
Amit Kumar <itsdevshady@gmail.com>
2025-11-14 15:07:08 +0530

    change login logout text on button

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
+  const [userLoginStatus, setUserLoginStatus] = useState(1);
+  const userStatus = {
+    LOGIN: 0,
+    LOGOUT: 1,
+  };
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -12,6 +19,17 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button
+              onClick={() => {
+                setUserLoginStatus(
+                  (userLoginStatus) => (userLoginStatus + 1) % 2
+                );
+              }}
+            >
+              {userLoginStatus === userStatus.LOGIN ? "Log in" : "Log out"}
+            </button>
+          </li>
         </ul>
       </div>
     </div>
